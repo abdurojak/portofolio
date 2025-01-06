84c6fccc13b78134259a422a2a4442641a9f6e29
abdurojak <abdurojak27@gmail.com>
2025-01-07 00:12:01 +0700

    07 Januari 2024 - Make Json for Data

---
+++ js/biodata.js
@@ -0,0 +1,14 @@
+fetch('../json/biodata.json')
+    .then(response => response.json())
+    .then(profileData => {
+        const profileList = document.getElementById('profile-list');
+        const aboutMe = document.getElementById('about-me');
+        profileList.innerHTML = `
+        <li><strong>Full Name </strong>: ${profileData.fullName}</li>
+        <li><strong>Place of Birth </strong>: ${profileData.placeOfBirth}</li>
+        <li><strong>Date of Birth</strong>: ${profileData.dateOfBirth}</li>
+        <li><strong>Domicile</strong>: ${profileData.domicile}</li>
+        <li><strong>Email </strong>: ${profileData.email}</li>
+    `;
+        aboutMe.innerHTML = `${profileData.aboutMe}`
+    }).catch(error => console.error('Error loading profile data:', error));
